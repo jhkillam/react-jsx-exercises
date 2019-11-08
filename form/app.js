91529507623f88968f66626f192603fda9767d35
Joey Killam <jhkillam@gmail.com>
2019-11-07 21:29:05 -0600

    finished form and list-group

--- form/app.js
+++ form/app.js
@@ -1,18 +1,48 @@
 let TextInput = () => {
-    return <span>Make this component render a styled text input tag</span>
+    return <span>
+      <div className="input-field col s6">
+        <input type="text" id="textbox"></input>
+        <label for="textbox">Text goes here</label>
+      </div>
+    </span>
 }
 
 let YesNoRadio = () => {
-    return <span>Make this component render two styled radio button side by side labelled "yes" and "no"</span>
+    return <span>
+      {/* Make this component render two styled radio button side by side labelled "yes" and "no" */}
+      <form>
+        <label>
+          <input name="group-1" type="radio" />
+          <span className="radio-text">Yes</span>
+        </label>
+        <label>
+          <input name="group-1" type="radio" />
+          <span className="radio-text">No</span>
+        </label>
+      </form>
+      <br/>
+      </span>
 }
 
 let SubmitButton = () => {
-    return <span>Make this component render a styled button of type "submit"</span>
+    return <span>
+      {/* Make this component render a styled button of type "submit" */}
+        <a className="waves-effect waves-light btn">Submit</a>
+      </span>
 }
 
 let Form = () => {
-    return <span>Make this Component render a form tag with 3 text inputs, two yes/no radio buttons, and a submit button </span>
+    return <span>
+      <div className="container">
+      {/* Make this Component render a form tag with 3 text inputs, two yes/no radio buttons, and a submit button  */}
+        <TextInput/>
+        <TextInput/>
+        <TextInput/>
+        <YesNoRadio/>
+        <YesNoRadio/>
+        <SubmitButton/>
+      </div>
+    </span>
 }
 
-
-ReactDOM.render(<Form />, document.getElementById('root'))
+ReactDOM.render(<Form/>, document.getElementById('root'))
