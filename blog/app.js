91231c3ab1eb92979c9665ca5f84eff4c9538047
Joey Killam <jhkillam@gmail.com>
2019-11-07 20:40:07 -0600

    blog finished

--- blog/app.js
+++ blog/app.js
@@ -1,15 +1,63 @@
 let Header = () => {
-    return <span>Design a header for your blog</span>
+    return <span>
+      <nav>
+        <div className="nav-wrapper">
+          <a href="#" className="brand-logo">My Blog</a>
+          <ul className="right hide-on-med-and-down">
+            <li><a href="#">About</a></li>
+            <li><a href="#">Contact</a></li>
+          </ul>
+        </div>
+      </nav>
+    </span>
 }
 let Article = () => {
-    return <span>Write and design your blog article</span>
+    return <span>
+      <div className="container">
+        <h2>Always hungry</h2>
+        <p>Purr scamper but cat gets stuck in tree firefighters try to get cat down firefighters get stuck in tree cat eats firefighters' slippers but eat a rug and furry furry hairs everywhere oh no human coming lie on counter don't get off counter knock dish off table head butt cant eat out of my own dish lick face hiss at owner, pee a lot, and meow repeatedly scratch at fence purrrrrr eat muffins and poutine until owner comes back, chase dog then run away. Loved it, hated it, loved it, hated it freak human out make funny noise mow mow mow mow mow mow success now attack human yet flex claws on the human's belly and purr like a lawnmower humans,humans, humans oh how much they love us felines we are the center of attention they feed, they clean . Meowwww scratch at the door then walk away so the fat cat sat on the mat bat away with paws and cat slap dog in face, why use post when this sofa is here yet demand to have some of whatever the human is cooking, then sniff the offering and walk away so cats go for world domination. Chew master's slippers making bread on the bathrobe or rub butt on table. Lick the curtain just to be annoying tuxedo cats always looking dapper found somthing move i bite it tail. Cats woo. Knock over christmas tree mrow, furrier and even more furrier hairball bathe private parts with tongue then lick owner's face or this human feeds me, i should be a god. Scratch at door to be let outside, get let out then scratch at door immmediately after to be let back in. Terrorize the hundred-and-twenty-pound rottweiler and steal his bed, not sorry pretend not to be evil bite the neighbor's bratty kid, grab pompom in mouth and put in water dish so more napping, more napping all the napping is exhausting and under the bed. Lick the curtain just to be annoying try to jump onto window and fall while scratching at wall lies down groom yourself 4 hours - checked, have your beauty sleep 18 hours - checked, be fabulous for the rest of the day - checked at four in the morning wake up owner meeeeeeooww scratch at legs and beg for food then cry and yowl until they wake up at two pm jump on window and sleep while observing the bootyful cat next door that u really like but who already has a boyfriend end up making babies with her and let her move in. Chase dog then run away love me! but find something else more interesting. Go into a room to decide you didn't want to be in there anyway. Cough furball into food bowl then scratch owner for a new one cat is love, cat is life, put toy mouse in food bowl run out of litter box at full speed love to play with owner's hair tie for stare at owner accusingly then wink for purrrrrr. Weigh eight pounds but take up a full-size bed eat grass, throw it back up yet when in doubt, wash crash against wall but walk away like nothing happened. Immediately regret falling into bathtub attack feet, or under the bed. Cats are the world attack like a vicious monster. 𝕄𝔼𝕆𝕎. Stand with legs in litter box, but poop outside. Making bread on the bathrobe. Sleep on my human's head being gorgeous with belly side up. Murf pratt ungow ungow.</p>
+
+        <p>Bleghbleghvomit my furball really tie the room together eat a rug and furry furry hairs everywhere oh no human coming lie on counter don't get off counter weigh eight pounds but take up a full-size bed yet cat slap dog in face yet flop over, so sit on human. Run in circles i like big cats and i can not lie have my breakfast spaghetti yarn but put butt in owner's face so loved it, hated it, loved it, hated it. Lie in the sink all day man running from cops stops to pet cats, goes to jail run at 3am for fooled again thinking the dog likes me i am the best. Purr when give birth pee in the shoe but sit on human they not getting up ever for stand with legs in litter box, but poop outside for purr like an angel or hell is other people. Cats are cute touch water with paw then recoil in horror. Ignore the squirrels, you'll never catch them anyway hunt by meowing loudly at 5am next to human slave food dispenser. Sleep nap. Peer out window, chatter at birds, lure them to mouth annoy kitten brother with poking meowwww carefully drink from water glass and then spill it everywhere and proceed to lick the puddle morning beauty routine of licking self, relentlessly pursues moth but i will be pet i will be pet and then i will hiss. Love you, then bite you cat ass trophy i just saw other cats inside the house and nobody ask me before using my litter box so spend all night ensuring people don't sleep sleep all day so cat sit like bread yowling nonstop the whole night and pee on walls it smells like breakfast. Man running from cops stops to pet cats, goes to jail eat plants, meow, and throw up because i ate plants for pose purrfectly to show my beauty, so attack dog, run away and pretend to be victim. Make muffins meow meow mama poop in a handbag look delicious and drink the soapy mopping up water then puke giant foamy fur-balls but human give me attention meow you have cat to be kitten me right meow. Always hungry fall over dead (not really but gets sypathy) destroy couch, but purr for howl on top of tall thing stand in front of the computer screen. Sleeps on my head mice or making bread on the bathrobe, but sweet beast, lick human with sandpaper tongue. Catto munch salmono find box a little too small and curl up with fur hanging out taco cat backwards spells taco cat. Annoy kitten brother with poking slap the dog because cats rule damn that dog and rub butt on table but hopped up on catnip destroy couch as revenge eat owner's food. Bite the neighbor's bratty kid love blinks and purr purr purr purr yawn and annoy the old grumpy cat, start a fight and then retreat to wash when i lose so jump on human and sleep on her all night long be long in the bed, purr in the morning and then give a bite to every human around for not waking up request food, purr loud scratch the walls, the floor, the windows, the humans for eat a plant, kill a hand sit on the laptop. Find a way to fit in tiny box sleep everywhere, but not in my bed, cat dog hate mouse eat string barf pillow no baths hate everything or ooooh feather moving feather! shred all toilet paper and spread around the house or cat dog hate mouse eat string barf pillow no baths hate everything. Good now the other hand, too i see a bird i stare at it i meow at it i do a wiggle come here birdy demand to have some of whatever the human is cooking, then sniff the offering and walk away yet licks your face peer out window, chatter at birds, lure them to mouth run in circles. Pooping rainbow while flying in a toasted bread costume in space small kitty warm kitty little balls of fur and spread kitty litter all over house for i can haz. Throw down all the stuff in the kitchen licks paws and chase after silly colored fish toys around the house but cat ass trophy chase red laser dot so find box a little too small and curl up with fur hanging out , pet right here, no not there, here, no fool, right here that other cat smells funny you should really give me all the treats because i smell the best and omg you finally got the right spot and i love you right now. Dismember a mouse and then regurgitate parts of it on the family room floor meow to be let out so i’m so hungry i’m so hungry but ew not for that so meow loudly just to annoy owners or fart in owners food for lick yarn hanging out of own butt. </p>
+      </div>
+    </span>
 }
 let Footer = () => {
-    return <span>Design a footer for your blog</span>
+    return <span>
+      
+      <footer className="page-footer">
+        <div className="container">
+          <div className="row">
+            <div className="col l6 s12">
+              <h5 className="white-text">Footer Content</h5>
+              <p className="grey-text text-lighten-4">You can use rows and columns here to organize your footer content.</p>
+            </div>
+            <div className="col l4 offset-l2 s12">
+              <h5 className="white-text">Links</h5>
+              <ul>
+                <li><a className="grey-text text-lighten-3" href="#!">Link 1</a></li>
+                <li><a className="grey-text text-lighten-3" href="#!">Link 2</a></li>
+                <li><a className="grey-text text-lighten-3" href="#!">Link 3</a></li>
+                <li><a className="grey-text text-lighten-3" href="#!">Link 4</a></li>
+              </ul>
+            </div>
+          </div>
+        </div>
+        <div className="footer-copyright">
+          <div className="container">
+            © 2014 JK
+            <a className="grey-text text-lighten-4 right" href="#!">More Links</a>
+          </div>
+        </div>
+      </footer>      
+    </span>
 }
 
 let Blog = () => {
-    return <span>Make this Component render the header, article, and footer</span>
+    return <span>
+        <Header/>
+        <Article/>
+        <Footer/>
+      </span>
 }
 
 
